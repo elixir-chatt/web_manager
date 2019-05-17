4456562b36f29681079fac74e799e971f6a0d683
Bruce Tate <bruce@rapidred.com>
2019-05-16 20:17:41 -0400

    UPdated

--- src/lib/web_manager_web/live/photo.ex
+++ src/lib/web_manager_web/live/photo.ex
@@ -4,15 +4,10 @@ defmodule WebManagerWeb.Photo do
   def render(assigns) do
     ~L"""
     <section class="phx-hero">
-      <h1>Submited Pictures from <%= @name %></h1>
+      <h1>Submited Pictures from <%= @person.name %></h1>
       <div class="row">
         <div class="col-xs-6 col-md-3">
-          <a href="#" phx-click="photo" phx-value="photo1" class="thumbnail">
-            <img height=100 src="/images/1.jpg" />
-          </a>
-          <a href="#" phx-click="photo" phx-value="photo2" class="thumbnail">
-            <img height=100 src="/images/1.jpg" />
-          </a>
+          <%= render_photos(@photos) |> Phoenix.HTML.raw %>
         </div>
       </div>
     </section>
@@ -59,7 +54,28 @@ defmodule WebManagerWeb.Photo do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, name: "Grace", clicked_photo: "---")}
+    {:ok, assign(socket, person: %{name: "Grace"}, photos: photos(), clicked_photo: "---")}
+  end
+  
+  def photos() do
+    [
+      photo("1.jpg", 1), photo("2.jpg", 2)
+    ]
+  end
+  
+  def photo(path, id), do: %{path: path, id: id}
+  
+  def render_photos(photos) do
+    photos
+    |> Enum.map(&render_photo/1)
+  end
+    
+  def render_photo(photo) do
+    """
+    <a href="#" phx-click="photo" phx-value="photo#{photo.id}" class="thumbnail">
+      <img height=100 src="/images/#{photo.path}" />
+    </a>
+    """
   end
   
   def handle_event("photo", clicked, socket) do
