f40a3f7b74ef96f518d11f49aa47016056bd5063
Bruce Tate <bruce@rapidred.com>
2019-06-13 19:29:14 -0400

    Accept, Reject, Random Photo

--- src/lib/web_manager_web/live/photo.ex
+++ src/lib/web_manager_web/live/photo.ex
@@ -4,21 +4,22 @@ defmodule WebManagerWeb.Photo do
   def render(assigns) do
     ~L"""
     <section class="phx-hero">
-      <h1>Submited Pictures from <%= @person.name %></h1>
+      <h1>Submitted Pictures from our Photobooth by <%= @person.name %></h1>
       <div class="row">
         <div class="col-xs-6 col-md-3">
           <%= render_photos(@photos) |> Phoenix.HTML.raw %>
         </div>
       </div>
     </section>
-    <h1><%= @clicked_photo %></h1>
+    <h1>accepted: <%= @accepted_photo_id%> 
+    rejected: <%= @rejected_photo_id%> </h1>
     <section>
           <form method="POST" action="uploadFile" enctype="multipart/form-data">
           <strong>Upload file:</strong> 
           <input type="file" name="file" />
               <input type="submit" value="Upload" />
           </form>
-
+          <a href="#" class="btn" phx-click="send-fake-photo">Send fake photo</a>
     </section>
     <section class="row">
       <article class="column">
@@ -54,16 +55,33 @@ defmodule WebManagerWeb.Photo do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, person: %{name: "Grace"}, photos: photos(), clicked_photo: "---")}
+    {
+      :ok, 
+      assign(
+        socket, 
+        person: %{name: "Lucas"}, 
+        photos: photos(), 
+        clicked_photo: "---", 
+        accepted_photo_id: "---", 
+        rejected_photo_id: "---")
+      }
   end
   
   def photos() do
-    [
-      photo("1.jpg", 1), photo("2.jpg", 2)
-    ]
+    []
   end
   
   def photo(path, id), do: %{path: path, id: id}
+  
+  def random_photo() do
+    number = :random.uniform(5)
+    %{path: "#{number}.jpg", id: number}
+  end
+  
+  def add_random_photo(socket) do
+    photos = socket.assigns.photos
+    assign(socket, :photos, [random_photo()|photos])
+  end
   
   def render_photos(photos) do
     photos
@@ -72,13 +90,36 @@ defmodule WebManagerWeb.Photo do
     
   def render_photo(photo) do
     """
-    <a href="#" phx-click="photo" phx-value="photo#{photo.id}" class="thumbnail">
+    <a href="#" phx-click="reject" phx-value="photo#{photo.id}" class="thumbnail">
       <img height=100 src="/images/#{photo.path}" />
     </a>
     """
   end
   
+  def accept(clicked, socket) do
+    assign(socket, accepted_photo_id: clicked)
+  end
+  
+  def reject(clicked, socket) do 
+    assign(socket, rejected_photo_id: clicked)
+  end
+  
   def handle_event("photo", clicked, socket) do
    {:noreply, assign(socket, clicked_photo: clicked )}
   end
+  
+  def handle_event("accept", clicked, socket) do
+  {:noreply, accept(clicked, socket) |> IO.inspect}
+  end
+  
+  def handle_event("reject", clicked, socket) do
+    {:noreply, reject(clicked, socket)}
+  end
+  
+  def handle_event("send-fake-photo", clicked, socket) do
+     {:noreply, add_random_photo(socket)}
+  end
+  
+  
+  
 end
