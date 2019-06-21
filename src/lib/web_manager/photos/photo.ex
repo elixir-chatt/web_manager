836aed1f8d1ed8c06b5bba2819d3e05704c84073
Seth Horsley <isethi@me.com>
2019-06-20 20:06:06 -0400

    added accept and reject functionlaity

--- src/lib/web_manager/photos/photo.ex
+++ src/lib/web_manager/photos/photo.ex
@@ -1,9 +1,11 @@
 defmodule WebManager.Photos.Photo do
   defstruct [:path, :id, troll: false, status: :pending ]
-  
+
   def accept(photo) do
+    %{photo | status: :accepted}
   end
 
   def reject(photo) do
+    %{photo | status: :rejected}
   end
 end
