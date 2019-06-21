ff07079d421e4e30fd3d3dbba7fd3a50488a6ea8
Grace McPherson <grace.theprogrammer@gmail.com>
2019-06-20 20:06:36 -0400

    photo_list functionality

--- src/lib/web_manager/photos/photo_list.ex
+++ src/lib/web_manager/photos/photo_list.ex
@@ -1,24 +1,33 @@
 defmodule WebManager.Photos.PhotoList do
   defstruct( photos: [] )
-  
+
   def show_photos(photos, :all), do: []
   def show_photos(photos, :rejected), do: []
   def show_photos(photos, :accepted), do: []
   def show_photos(photos, :pending), do: []
-  
+
   def accept(photos, id) do
-    # find...
-    # accept...
-    # replace...
+    accepted_photo = photos
+    |> Enum.find(fn x -> x.id == id end)
+    |> Photo.accept
+
+    index = Enum.find_index(photos, fn x -> x.id == id end)
+    List.replace_at(photos, index, accepted_photo)
   end
 
 
   def reject(photos, id) do
-    # find...
-    # reject...
-    # replace...
+    rejected_photo = photos
+    |> Enum.find(fn x -> x.id == id end)
+    |> Photo.reject
+
+    index = Enum.find_index(photos, fn x -> x.id == id end)
+    List.replace_at(photos, index, rejected_photo)
   end
-  
+
   def next_photo(photos, photo) do
+    current_photo_index = Enum.find_index(photos, fn x -> x == photo end)
+    Enum.fetch!(photos, current_photo_index+1)
+    #TODO wrap around the list if last photo
   end
 end
