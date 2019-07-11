b0fc2754893df74a23ab33b53e9f7030ee405600
Bruce Tate <bruce@rapidred.com>
2019-07-11 17:56:32 -0400

    configuration of aws

--- src/config/dev.exs
+++ src/config/dev.exs
@@ -88,3 +88,7 @@ config :web_manager, WebManager.Repo,
   database: "web_manager_dev",
   hostname: "localhost",
   pool_size: 10
+
+config :ex_aws,
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "default", 30}, :instance_role],
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, {:awscli, "default", 30}, :instance_role]
