def7d16724f83e05c2022153a6656c4301764f53
Kai Janson <kaijanson@MacBook-Air.localdomain>
2019-10-01 18:02:35 -0400

    Bumping elixir version to 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WebManager.MixProject do
     [
       app: :web_manager,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
