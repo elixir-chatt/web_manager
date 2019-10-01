6e42e70134b1754b109e6fc72575038e34e3200e
Kai Janson <kaijanson@MacBook-Air.localdomain>
2019-10-01 18:13:27 -0400

    bumping version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WebManager.MixProject do
   def project do
     [
       app: :web_manager,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
