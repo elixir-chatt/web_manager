7e170d6b8d814dbf7e134c3fc6671e9816e5b0fb
Bruce Tate <bruce@rapidred.com>
2019-10-08 16:24:44 -0400

    api auth

--- lib/web_manager/plugs/load_jpg.ex
+++ lib/web_manager/plugs/load_jpg.ex
@@ -7,10 +7,20 @@ defmodule WebManager.Plugs.LoadJpg do
   def call(conn, _opts) do
     {:ok, jpg, _more} = read_body conn
     headers = Map.new conn.req_headers
+    
+    
     conn
+    |> verify(headers, System.get_env("TAKE_FIVE_API_KEY"))
     |> assign(:troll, Map.fetch!(headers, "troll"))
     |> assign(:jpg, jpg)
     |> assign(:index, Map.fetch!(headers, "index"))
     |> assign(:group, Map.fetch!(headers, "gts"))
+  end
+  
+  def verify(conn, %{"api-key" => key}, key), do: conn
+  def verify(conn, _, _) do
+    conn
+    |> put_status(:unauthorized) 
+    |> halt
   end
 end
